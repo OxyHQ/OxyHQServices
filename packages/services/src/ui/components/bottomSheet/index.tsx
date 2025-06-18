d3ef8932ce5887acb02659e7fc281abc988f9f3e
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-06-18 06:37:06 +0200

    feat(services): migrate to gorhom bottom sheet

--- packages/services/src/ui/components/bottomSheet/index.tsx
+++ packages/services/src/ui/components/bottomSheet/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-// Export our custom bottom sheet components
-// This replaces the @gorhom/bottom-sheet dependency with our own implementation
-export { BottomSheetModal } from './BottomSheetModal';
-export type { BottomSheetModalRef, BottomSheetModalProps } from './BottomSheetModal';
-export { BottomSheetBackdrop } from './BottomSheetBackdrop';
-export type { BottomSheetBackdropProps } from './BottomSheetBackdrop';
-export { BottomSheetModalProvider } from './BottomSheetModalProvider';
-export type { BottomSheetModalProviderProps } from './BottomSheetModalProvider';
-export { BottomSheetView } from './BottomSheetView';
-export type { BottomSheetViewProps } from './BottomSheetView';
-export { BottomSheetScrollView } from './BottomSheetScrollView';
-export type { BottomSheetScrollViewProps } from './BottomSheetScrollView';
+// Re-export bottom sheet components from @gorhom/bottom-sheet
+export {
+  BottomSheetModal,
+  BottomSheetBackdrop,
+  BottomSheetModalProvider,
+  BottomSheetView,
+  BottomSheetScrollView,
+} from '@gorhom/bottom-sheet';
+
+export type {
+  BottomSheetModalProps,
+  BottomSheetBackdropProps,
+} from '@gorhom/bottom-sheet';
+export type { BottomSheetModalMethods as BottomSheetModalRef } from '@gorhom/bottom-sheet/lib/typescript/types';
